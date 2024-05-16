c09683d9e6a53ccfcf594c4a05d6e4dc5f0e1d39
Mr zakir ullah <mrzak802@gmail.com>
2024-05-16 13:09:57 -0700

    comment

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from "react-router-dom"
 
 function SignUp() {
+  const [user, setUser]=useState({
+    fullName:"",
+    username:"",
+    password:"",
+    confirmPassword:"",
+    gender:""
+  })
+  const onSubmitHandler =(e)=>{
+    e.preventDefault()
+    console.log(user)
+  }
+
   return (
     <div className='min-w-96 max-auto'>
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100">
         <h1 className="text-3xl font-bold text-center text-gray-300">SignUp</h1>
-        <form action="">
+        <form action="" onSubmit={onSubmitHandler}>
           <div>
             <label className='label p-2'>
               <span className="text-base label-text">Full Name</span>
             </label>
             <input type="text"
+            value={user.fullName}
+            onChange={(e)=>setUser({...user,fullName:e.target.value})}
              className='w-full input-bordered h-8 bg-white'
              placeholder='name'
              />
@@ -21,6 +35,8 @@ function SignUp() {
               <span className="text-base label-text">User Name</span>
             </label>
             <input type="text"
+            value={user.username}
+            onChange={(e)=>setUser({...user,username:e.target.value})}
              className='w-full input-bordered h-8 bg-white'
              placeholder='Userame'
              />
@@ -30,6 +46,8 @@ function SignUp() {
               <span className="text-base label-text">Password</span>
             </label>
             <input type="password"
+            value={user.password}
+            onChange={(e)=>setUser({...user,password:e.target.value})}
              className='w-full input-bordered h-8 bg-white'
              placeholder='password'
              />
@@ -39,6 +57,8 @@ function SignUp() {
               <span className="text-base label-text">Conform Password</span>
             </label>
             <input type="password"
+            value={user.confirmPassword}
+            onChange={(e)=>setUser({...user,confirmPassword:e.target.value})}
              className='w-full input-bordered h-8  bg-white'
              placeholder='Conform password'
              />
@@ -55,7 +75,7 @@ function SignUp() {
           </div>
               <p className='text-center'>Already have an account ? <Link to="/login"> Login</Link></p>
           <div>
-            <button className='btn btn-block btn-sm mt-2 border border-slate-700'>SignUp</button>
+            <button type='submit' className='btn btn-block btn-sm mt-2 border border-slate-700'>SignUp</button>
           </div>
         </form>
       </div>
