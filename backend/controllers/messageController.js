839e4aee73709692d07746367a39cebacb37352a
Mr zakir ullah <mrzak802@gmail.com>
2024-05-29 03:07:33 -0700

    comment

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,32 +1,40 @@
-import {Conversation} from "../models/conversationModel.js"
+import { Conversation } from "../models/conversationModel.js";
 import { Message } from "../models/messageModel.js";
-export const sendMessage = async(req,res)=>{
-    try {
-        const senderId = req.id;
-        const recieverId= req.param.id;
-        const {message} = req.body;
-        let gotConversation = await Conversation.findOne({
-            participants:{$all :[senderId,recieverId]},
-        });
-        if (!gotConversation) {
-            gotConversation = await Conversation.create({
-                participants:[senderId, recieverId]
-            })
-        }
-        const newMessage = await Message.create({
-            senderId,
-            recieverId,
-            message,
-        })
-        if (newMessage) {
-            gotConversation.messages.push(newMessage._id)
-        };
-        await gotConversation.save();
-        
-        return res.status(201).json({
-            message:"message send successfully"
-        })
-    } catch (error) {
-        console.log(error)
+
+export const sendMessage = async (req, res) => {
+  try {
+    const senderId = req.id;
+    const receiverId = req.params.id; // Corrected this line
+    const { message } = req.body;
+
+    let gotConversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!gotConversation) {
+      gotConversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
     }
-}
+
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      gotConversation.messages.push(newMessage._id);
+      await gotConversation.save();
+    }
+
+    return res.status(201).json({
+      message: "Message sent successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "An error occurred while sending the message",
+    });
+  }
+};
